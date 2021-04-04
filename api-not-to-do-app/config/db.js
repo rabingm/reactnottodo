0b5211000a089487dde73b66293dba26e1e05113
Rabin Ghimire <rabinghimire2012@gmail.com>
2021-04-04 22:11:39 +1000

    test

--- api-not-to-do-app/config/db.js
+++ api-not-to-do-app/config/db.js
@@ -7,7 +7,7 @@ const mongoClient = async () => {
 	// const connStr = process.env.PROD_MONGO_CLIENT
 	
 
-	const connStr = process.env.NODE_ENV ==="production" ? process.env.PROD_MONGO_CLIENT : process.env.MONGO_CLIENT
+	const connStr =  process.env.MONGO_CLIENT
 
 	try {
 		const con = await mongoose.connect(connStr, {
